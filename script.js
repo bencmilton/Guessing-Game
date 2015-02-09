2c70f2f5146720761c0f3a0369a33f417b43cb5d
Ben Milton <ben.c.milton@gmail.com>
2015-02-09 00:37:57 -0500

    Updated styling and functionality

--- script.js
+++ script.js
@@ -1,30 +1,134 @@
-var randomNumber = 0;
+//Function takes user input and compares number to numToGuess
+var turn = 0;
+var guessesArr = [];
+var newGuessSwitch = false;
+var newGuess;
+var userWonGame = false;
 
-//start new game and pick a new randomly generated number
-function new_game (){
-	randomNumber = Math.floor(Math.random() * 100) + 1;
-}
+function yourGuess() {
+    if (newGuessSwitch === true){
+    	guess = newGuess;
+    	newGuessSwitch = false;
+    } else {
+    	guess = document.getElementById("guess").value;
+    }
 
+    guesses = document.getElementById("output");
 
+//Check to see if answer was already submitted (if the guess array has at least one entry)
+	if (guessesArr.length > 0 && guessesArr.indexOf(guess) > -1){
+		
+		alertify.prompt("You have already guessed "+guess+". Please pick another number.", function (ee, str) {
+			if (ee) {
+				newGuess = str;
+				newGuessSwitch = true;
+				yourGuess();
 
-//compare number function
-function compare_number (num){
-	if(num === randomNumber){
-		alert("You got it!");
-		} else if (num > randomNumber) {
-			alert("You're too high!");
-		} else if (num < randomNumber) {
-			alert("You're too low!");
+				} else {
+					//user clicked "cancel"
+				}
+		}, "New guess");		
+	} else {
+
+//Turn counter
+	turn++;
+
+//Checks to make sure input is a number 1 and 100
+    if(isNaN(guess) || guess < 1 || guess > 100){
+    	alertify.alert(guess+" is not a number between 1 and 100! Please guess again.");
+    } else {
+
+//store guess in guessArr
+		guessesArr.push(guess);
+
+//if numbers match    	
+	    if (guess == numToGuess) {
+	        guesses.value = guesses.value + "\r" + "("+guess+") is the right number! ";
+	        userWonGame = true;
+	        document.getElementById("jumbo").style.backgroundColor = "green";
+	        alertify.confirm("Congrats you picked the right number "+numToGuess+"! Click OK to play again.", function(e){
+	        	if (e){	        			        		
+	        			location.reload();	        			
+	        		}
+	        	});
+		    } 
+
+//guess is greater than hidden number
+		    else if (guess > numToGuess && Math.abs(parseInt(guess - numToGuess)) > 30) {
+		        guesses.value = guesses.value + "\r" + "You're way off. ("+guess+") is far too high! (Turn "+turn+").";
+		    } 
+
+		    else if (guess > numToGuess && Math.abs(parseInt(guess - numToGuess)) > 15) {
+		        guesses.value = guesses.value + "\r" + "You're getting warmer, but ("+guess+") is too high! (Turn "+turn+").";
+		    } 
+
+		    else if (guess > numToGuess && Math.abs(parseInt(guess - numToGuess)) > 5) {
+		        guesses.value = guesses.value + "\r" + "You're getting hotter! ("+guess+") is a bit too high! (Turn "+turn+").";
+		    } 
+
+		    else if (guess > numToGuess && Math.abs(parseInt(guess - numToGuess)) < 6) {
+		        guesses.value = guesses.value + "\r" + "You're so close! ("+guess+") is a teeny bit too high! (Turn "+turn+").";
+		    } 
+
+//guess is less than hidden number
+		   	else if (guess < numToGuess && Math.abs(parseInt(guess - numToGuess)) > 30) {
+		        guesses.value = guesses.value + "\r" + "You're way off. ("+guess+") is far too low! (Turn "+turn+").";
+		    } 
+
+		    else if (guess < numToGuess && Math.abs(parseInt(guess - numToGuess)) > 15) {
+		        guesses.value = guesses.value + "\r" + "You're getting warmer, but ("+guess+") is too low! (Turn "+turn+").";
+		    } 
+
+		    else if (guess < numToGuess && Math.abs(parseInt(guess - numToGuess)) > 5) {
+		        guesses.value = guesses.value + "\r" + "You're getting hotter! ("+guess+") is a bit too low! (Turn "+turn+").";
+		    } 
+
+		    else if (guess < numToGuess && Math.abs(parseInt(guess - numToGuess)) < 6) {
+		        guesses.value = guesses.value + "\r" + "You're so close! ("+guess+") is a teeny bit too low! (Turn "+turn+").";
+		    } 
 		}
 	}
-};
 
-//end the game and reveal the number
-function i_give_up (){
-	alert("Sorry you lost! The number was " + randomNumber + ".");
+
+		
+//Alert and end game if player uses 5 turns
+		if (turn === 5 && userWonGame === false){
+			document.getElementById("jumbo").style.backgroundColor = "red";
+			alertify.confirm("Sorry, you are out of turns! The number was "+numToGuess+".", function (youLose){
+				if (youLose){
+					location.reload();
+				}
+			});					
+		}
+	}
+
+//Cheat function gives user the hidden number
+function showNumberToGuess() {
+    if (document.getElementById('cheat')) {
+        document.getElementById('numberToGuess').value = numToGuess;
+        document.getElementById('cheatShow').style.display = 'inline';
+    } else {
+        document.getElementById('numberToGuess').value = '';
+        document.getElementById('cheatShow').style.display = 'none';
+    }
 }
 
-function testResults (form) {
-    var TestVar = form.inputbox.value;
-    alert ("You typed: " + TestVar);
+//"New game" button alerts user and reloads page
+function newGame () {
+	alertify.confirm("Click OK to start a new game.", function (newGameConfirm){
+		if (newGameConfirm){
+			location.reload();
+			}
+		});	
 }
+
+//Random number generator (1-100)
+var numToGuess = Math.floor(Math.random()*100);
+
+
+
+
+
+
+
+
